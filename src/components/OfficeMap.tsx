e9e61b251223d30745b67de53eef0d8344cb55d3
Iggy Love <hey@iggy.love>
2025-09-12 13:59:01 +0200

    Update map tile layers and attributions across CityMap, OfficeMap, and SchoolMap components for improved visual consistency and accuracy.

--- src/components/OfficeMap.tsx
+++ src/components/OfficeMap.tsx
@@ -122,8 +122,8 @@ export default function OfficeMap() {
         dragging={true}
       >
         <TileLayer
-          url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-          attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
         />
         {officeLocations.map((location) => (
           <Marker
